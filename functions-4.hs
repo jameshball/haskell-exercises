fafca1367f5f3d69801aa5a8f9322edc60e7bfcc
James Ball <balljames63@gmail.com>
2019-10-03 13:28:15 +0100

    Improved readability of some code.

--- functions-4.hs
+++ functions-4.hs
@@ -3,6 +3,7 @@ import Data.Char
 main = do
     print (triangleArea (0, 0) (23, 11) (21,8))
 
+-- Type synonymn
 type Vertex = (Float, Float)
     
 distance :: Vertex -> Vertex -> Float
@@ -10,5 +11,10 @@ distance (x1, y1) (x2, y2)
     = sqrt ((x1 - x2)^2 + (y1 - y2)^2)
 
 triangleArea :: Vertex -> Vertex -> Vertex -> Float
-triangleArea x y z
-    = let (a, b, c) = (distance x y, distance x z, distance y z) in let s = (a + b + c)/2 in sqrt (s * (s - a) * (s - b) * (s - c))
+triangleArea x y z =
+    sqrt (s * (s - a) * (s - b) * (s - c))
+    where
+        s = (a + b + c)/2
+        a = distance x y
+        b = distance x z
+        c = distance y z
--- functions-5.hs
+++ functions-5.hs
@@ -1,8 +1,9 @@
 import Data.Char
 
 main = do
-    print (isPrime 49)
- 
+    print (isPrime 47)
+
 isPrime :: Int -> Bool
 isPrime a
-    = if a > 1 then if [x | x <- [2 .. round $ sqrt $ fromIntegral a], mod a x == 0] == [] then True else False else False
+    | a > 1      = [x | x <- [2 .. round $ sqrt $ fromIntegral a], mod a x == 0] == []
+    | otherwise  = False
