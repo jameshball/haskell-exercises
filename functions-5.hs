a62695cd95bca2acb805cd8f22077385e446ec77
James Ball <balljames63@gmail.com>
2019-10-06 22:22:42 +0100

    Added very nice solution to 2.5

--- functions-5.hs
+++ functions-5.hs
@@ -5,3 +5,13 @@ isPrime :: Int -> Bool
 isPrime a
     | a > 1      = [x | x <- [2 .. round $ sqrt $ fromIntegral a], mod a x == 0] == []
     | otherwise  = False
+
+-- Lovely solution to this by William Grant:
+{--
+primes = [x | x <- [2..], isPrime x]
+
+isPrime :: Int -> Bool
+isPrime 2 = True
+isPrime x = [y | y <- (takeWhile ((<=) limit) primes), mod x y == 0] == []
+    where limit = floor $ sqrt $ fromIntegral x
+--}
